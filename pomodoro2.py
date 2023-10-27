19d5ff5acf01f0676f140df69526cb5096d664ca
Ezra Jeonadab G. Del Rosario <128983643+oji-py@users.noreply.github.com>
2023-10-27 23:08:03 +0800

    Update pomodoro2.py

--- pomodoro2.py
+++ pomodoro2.py
@@ -7,23 +7,23 @@ from PIL import Image, ImageOps
 st.set_page_config(page_title = "Pomodoro Technique", layout = "wide", initial_sidebar_state = "collapsed")
 
 class Pomodoro:
-    # def autoplay_audio(self, file_path: str):
-    #     sound = st.empty()
-    #     with sound:
-    #         with open(file_path, "rb") as f:
-    #             data = f.read()
-    #             b64 = base64.b64encode(data).decode()
-    #             md = f"""
-    #                 <audio autoplay="true">
-    #                 <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-    #                 </audio>
-    #                 """
-    #             st.markdown(
-    #                 md,
-    #                 unsafe_allow_html=True,
-    #             )
-    #     t.sleep(13)
-    #     sound.empty()
+    def autoplay_audio(self, file_path: str):
+        sound = st.empty()
+        with sound:
+            with open(file_path, "rb") as f:
+                data = f.read()
+                b64 = base64.b64encode(data).decode()
+                md = f"""
+                    <audio autoplay="true">
+                    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                    </audio>
+                    """
+                st.markdown(
+                    md,
+                    unsafe_allow_html=True,
+                )
+        t.sleep(13)
+        sound.empty()
     def study(self, study=25*60):
         header = st.empty()
         pom = st.empty()
